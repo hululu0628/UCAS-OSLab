ff51f44b35c8d398095ab5c267ebda944a705b03
Xianan Zhu <zhuxianan22@mails.ucas.ac.cn>
2024-12-11 01:24:26 +0000

    p5: add new comments

--- include/mode.h
+++ include/mode.h
@@ -1,12 +1,12 @@
 #ifndef INCLUDE_MODE_H
 #define INCLUDE_MODE_H
 
-#define M_CORE		// enable multi core
+#define M_CORE		// enable multi-core
 
 #define QEMU_TEST	// test net device in qemu
 
 #define TXQE_TEST	// test e1000-txqe exception
 
-#define NET_DEVICE	// for prj5
+#define NET_DEVICE	// for prj5, enable when using run-net
 
 #endif
